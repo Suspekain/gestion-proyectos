f9cdf99b91a4ccc7564599ecbf9c69560eab48f5
Ekain Susperregi <suspekain@gmail.com>
2019-12-10 16:54:00 +0100

    dep + empl + pro estable

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -12,22 +12,18 @@
     <th>Nombre</th>
     <th>Email</th>
     <th>Telefono</th>
-    <th>Responsable de proyecto</th>
     <th>Departamento</th>
   </tr>
 
-  @if (isset($empleados))
   @foreach ($empleados as $empleado)
   <tr>
     <td><a href="{{route('empleado.show', $empleado->id)}}">{{$empleado->id}}</a></td>
     <td>{{$empleado->nombre}}</td>
     <td>{{$empleado->email}}</td>
     <td>{{$empleado->telefono}}</td>
-    <td><a href="{{route('proyecto.show', $empleado->proyecto->id)}}">{{$empleado->proyecto->id}}</a></td>
-    <td><a href="{{route('departamento.show', $empleado->departamento->id)}}">{{$empleado->departamento->id}}</a></td>
+    <td><a href="{{route('departamentos.index')}}">{{$empleado->departamento_id}}</a></td>
   </tr>
   @endforeach
-  @endif
 
 </table>
 
