707b64a52c4985d9321a02ce189e3d253f59b3a0
Ekain Susperregi <suspekain@gmail.com>
2019-12-10 17:15:38 +0100

    empleados-proyectos taula+seed

--- database/seeds/Empleados.php
+++ database/seeds/Empleados.php
@@ -12,13 +12,6 @@ class Empleados extends Seeder
   */
   public function run() {
     for ($i=0; $i < 20; $i++) {
-      DB::table('empleados')->insert([
-        'nombre' => Str::random(10),
-        'apellido' => Str::random(15),
-        'email' => 'email@domain.com',
-        'telefono'  => rand(600000000,999999999),
-        'departamento_id' => rand(1,5)
-      ]);
       factory(App\Empleado::class)->create();
     }
   }
